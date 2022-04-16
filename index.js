c43efb83c49a4d7b1bf4afa25ad4e448048c909e
annomao <omaoannkerubo@gmail.com>
2022-04-16 23:13:20 +0300

    commit tree d0c8ad9555bbac4efd8aa298e89fbc4a626347a3

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i=0;i<array.length;i++){
+   let num = array[i]
+    for(let j=i+1;j<array.length;j++){
+     const num2 = array[j]
+     const total = num + num2
+     if(total === target){
+       return true
+     }
+    }
+  }
+  return false
 }
-
 /* 
   Write the Big O time complexity of your function here
 */
